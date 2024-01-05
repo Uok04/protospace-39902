5914e17a96858b82c6d2669aa983744fc91384e1
Uok04 <usaya382@gmail.com>
2024-01-05 21:44:12 +0900

    コメントフォームを空のまま投稿すると、そのページに留まるように実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,10 +4,12 @@ class CommentsController < ApplicationController
   def create
     @prototype = Prototype.find(params[:prototype_id])
     @comment = @prototype.comments.build(comment_params)
+    @comment.user = current_user
   
     if @comment.save
       redirect_to prototype_path(@prototype)
     else
+      @comments = @prototype.comments.includes(:user)
       render 'prototypes/show'
     end
   end
